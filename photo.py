f47a22bc2f93c7cbc3b01114f9fee1502963c779
CEllmemichael <66575985+CEllmemichael@users.noreply.github.com>
2022-05-15 10:00:14 +0800

    Add files via upload

---
+++ photo.py
@@ -0,0 +1,12 @@
+import pyautogui
+import cv2
+import numpy as np
+import time
+time.sleep(1)
+img = pyautogui.screenshot(region=[500,300, 900, 470])  # 分别代表：左上角坐标，宽高
+#对获取的图片转换成二维矩阵形式，后再将RGB转成BGR
+#因为imshow,默认通道顺序是BGR，而pyautogui默认是RGB所以要转换一下，不然会有点问题
+img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("截屏",gray)
+cv2.waitKey(0)
