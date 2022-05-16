bd6e4f2bb5711a5effbdd29a18c438a1b3c3484a
CEllmemichael <66575985+CEllmemichael@users.noreply.github.com>
2022-05-16 19:34:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from aip import AipOcr
 from mails import mail
 from mails import loop
 from qqmsg import sendqqmasg,autoclick
-APP_ID = "26070078"
-API_KEY = "th9yqrTGYuvC3xGysTNYGghr"
-SECRET_KEY = "mFqiwk79WFfWBY1UroQE4v7kbSU7WYyU"
+APP_ID = "c"
+API_KEY = "c"
+SECRET_KEY = "c"
 import pyautogui
 import cv2
 import numpy as np
